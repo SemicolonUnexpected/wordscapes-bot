b34ca62f1ae62ee92e213e1f7c7fb4dee69fcca8
SemicolonUnexpected <m.gracey1409@gmail.com>
2024-07-14 20:47:10 +0000

    idek

--- vision.py
+++ vision.py
@@ -9,29 +9,42 @@ def get_wheel():
     height, width, channels = image.shape
 
     # Find the wheel
-    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    circles = cv.HoughCircles(gray_image,cv.HOUGH_GRADIENT,2,200,
+    grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    circles = cv.HoughCircles(grey_image,cv.HOUGH_GRADIENT,2,200,
                               param1=200,param2=150,minRadius=width//5,maxRadius=width//3)
 
 
     circles = circles[0]
     circles = np.uint16(circles)
 
+    # There should only be one circle
     assert len(circles) == 1
 
     circle = circles[0]
-
-    # Create an image with the wheel circle drawn on to it for debugging purposes
     center = (circle[0], circle[1])
-    # Circle center
-    cv.circle(image, center, 2, (0, 0, 255), 3)
-    # Circle outline
     radius = circle[2]
-    cv.circle(image, center, radius, (255, 0, 255), 3)
 
-    cv.imwrite("circled.png", image)
+    # Threshold the image
+    if grey_image[circle[1], circle[0]] == 0:
+        _, thresh = cv.threshold(grey_image, 250, 255, cv.THRESH_BINARY)
+    else:
+        _, thresh = cv.threshold(grey_image, 250, 255, cv.THRESH_BINARY_INV)
 
-    # Sample the center point on the wheel
-    # The wheel can be black or white
+    # Make a mask of everything but the wheel
+    mask = np.zeros((height, width), dtype="uint8")
+    cv.circle(mask, center, radius - 5, 255, -1)
+    letters = cv.bitwise_and(thresh, thresh, mask=mask)
+
+    # Find contours
+    contours, hierarchy = cv.findContours(letters, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    # Bounding rectangles
+    for cnt in contours:
+        x,y,w,h = cv.boundingRect(cnt)
+        cv.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
+    cv.imwrite("images/bounding.png", image)
+
+    cv.imwrite("images/mask-and-thresh.png", letters)
+
          
 get_wheel()
